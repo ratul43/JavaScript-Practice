40042764116be1630ad79ed7d63b6c455b395ff8
ratul43 <kmratul971@gmail.com>
2024-09-28 11:18:50 +0600

    Git master new file added

---
+++ js master/js object & json array strings js date math number window obj navigator/index.js
@@ -0,0 +1,14 @@
+//---------------- JS Math -----------------//
+
+
+// abs, ceil, floor, max, min, random, round, PI, pow
+console.log(Math.abs(-90))
+console.log(Math.PI)
+console.log((1000+Math.random()*9000))
+console.log(Math.pow(2,3))
+console.log(Math.sqrt(16))
+console.log(Math.min(1,3,6))
+console.log(Math.max(1,3,6))
+console.log(Math.round(4.7))
+console.log(Math.ceil(4.7))
+console.log(Math.floor(4.7))
