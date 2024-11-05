504f5e8f3968a4a16ff02065413615e723b96e7b
ratul43 <kmratul971@gmail.com>
2024-11-05 10:15:10 +0600

    Request response upload header

---
+++ js master/module 14/class 02/JS req, res, middleware/app/config/config.js
@@ -0,0 +1,31 @@
+import * as path from 'node:path';
+
+
+export const MONGODB_CONNECTION="";
+
+export const JWT_SECRET = "SELGKJEFSKLJ6574671FDDHF";
+export const JWT_EXPIRATION_TIME = 60*60*24*30;
+
+export const EMAIL_HOST ="";
+export const EMAIL_PORT ="";
+export const EMAIL_USER ="";
+export const EMAIL_PASSWORD ="";
+
+
+export const MAX_JSON_SIZE = "50mb";
+export const URL_ENCODED = true;
+
+export const REQUEST_LIMIT_TIME = 15*60*1000; // 15 Min
+export const REQUEST_LIMIT_NUMBER = 3000; // Per 15 Min 3000 Request Allowed
+
+
+
+export const WEB_CACHE=false;
+export const PORT=8000;
+
+
+export function UPLOAD_FOLDER(fileName){
+    return path.resolve(process.cwd(), 'storage', fileName);
+}
+
+
---
+++ js master/module 14/class 02/JS req, res, middleware/app/controllers/taskController.js
@@ -0,0 +1,53 @@
+// Create
+
+import {UPLOAD_FOLDER} from "../config/config.js";
+import {moveFile} from "../utility/moveFile.js";
+
+export const demo1 = async (req, res) =>{
+
+    // params
+    let name = req.params.name;
+    let age = req.params.age;
+    let city = req.params.city;
+
+
+    // query string
+    let country = req.query.country;
+    let language = req.query.language;
+
+
+    return res.json({message:language});
+
+}
+
+
+export const demo2 = async (req,res) =>{
+
+    //let reqBody = req.body;
+    let reqBody = req.body['firstname'];
+
+    return res.json({message:reqBody });
+
+}
+
+
+export const demo3 = async (req,res) => {
+
+    let reqBody = req.body;
+
+    return res.json({message: reqBody});
+}
+
+export const demo4 = async (req,res) => {
+    let myFile = req.files['myFile'];
+    let myFilePath = UPLOAD_FOLDER(myFile.name);
+    await moveFile(myFile,myFilePath);
+
+    return res.json({message:"Success moved file"});
+
+}
+
+export const demo5 = async (req,res) =>{
+    let reqHeaders = req.headers;
+    return res.json({message:reqHeaders});
+}
